36200e1587aadd4bee7f0d19cb25b462aa7e2432
Mateusz Zylka <mateuszzylkaa@gmail.com>
2018-02-04 17:15:59 +0100

    Add task_8_6

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var a = 2;
+var b = 3;
+var value = (a * a) - (2 * a * b) - (b * b);
+
+if (value > 0) {
+	console.log('The result is positive', value);
+}
+if (value < 0) {
+	console.log('The result is negative', value);
+}
+if (value == 0) {
+	console.log('The result is equal ', value);
+}
